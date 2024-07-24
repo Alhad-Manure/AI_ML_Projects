1d73a0acccf3b92cfe1eaf8cd72c668e3040272b
Alhad Manure <60777121+Alhad-Manure@users.noreply.github.com>
2024-07-24 22:58:47 +0530

    Add files via upload

---
+++ SupervisedLearning/SVM/SupportVectorMachineImpl.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import pylab as pl
+import numpy as np
+import scipy.optimize as opt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.metrics import classification_report, confusion_matrix
+import itertools
+from sklearn.metrics import f1_score
+from sklearn.metrics import jaccard_score
+
+cell_df = pd.read_csv("D:\Career\Programming\ML\SupervisedLearning\data\cell_samples.csv")
+
+# take a look at the dataset
+print("Data look-up: \n", cell_df.head())
+print("Data summerisation: \n", cell_df.describe())
+
+ax = cell_df[cell_df['Class'] == 4][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='DarkBlue', label='malignant');
+cell_df[cell_df['Class'] == 2][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='Yellow', label='benign', ax=ax);
+plt.show()
+
+print("Data columns' data types: \n", cell_df.dtypes)
+
+cell_df = cell_df[pd.to_numeric(cell_df['BareNuc'], errors='coerce').notnull()]
+cell_df['BareNuc'] = cell_df['BareNuc'].astype('int')
+print("Data columns' data types: \n", cell_df.dtypes)
+
+feature_df = cell_df[['Clump', 'UnifSize', 'UnifShape', 'MargAdh', 'SingEpiSize', 'BareNuc', 'BlandChrom', 'NormNucl', 'Mit']]
+X = np.asarray(feature_df)
+print("Processed feature data look-up: \n", X[0:5])
+
+y = np.asarray(cell_df['Class'])
+print("Label data look-up: \n", y [0:5])
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+
+clf = svm.SVC(kernel='rbf')
+clf.fit(X_train, y_train) 
+
+yhat = clf.predict(X_test)
+print("Model prediction look-up: \n", yhat[0:5])
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, yhat, labels=[2,4])
+np.set_printoptions(precision=2)
+
+print("classification report: \n", classification_report(y_test, yhat))
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['Benign(2)','Malignant(4)'],normalize= False,  title='Confusion matrix')
+
+print("f1 score: ", f1_score(y_test, yhat, average='weighted'))
+print("jaccard score: ", jaccard_score(y_test, yhat, pos_label=2))
+
+clfLin = svm.SVC(kernel='linear')
+clfLin.fit(X_train, y_train) 
+
+yhat = clfLin.predict(X_test)
+print("Model prediction look-up for Linear kernel function: \n", yhat[0:5])
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, yhat, labels=[2,4])
+np.set_printoptions(precision=2)
+
+print("classification report for Linear kernel function: \n", classification_report(y_test, yhat))
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['Benign(2)','Malignant(4)'],normalize= False,  title='Confusion matrix')
+
+print("f1 score for Linear kernel function: ", f1_score(y_test, yhat, average='weighted'))
+print("jaccard score for Linear kernel function: ", jaccard_score(y_test, yhat, pos_label=2))
+
+
+
